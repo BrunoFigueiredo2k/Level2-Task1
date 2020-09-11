f1a13a2239937d9d5eb69abed99dae74f53ae54c
@BrunoFigueiredo2k <brunofigueiredo@live.nl>
2020-09-11 14:00:59 +0200

    step 2.3

--- app/src/main/java/com/example/level2_task1/PlaceAdapter.kt
+++ app/src/main/java/com/example/level2_task1/PlaceAdapter.kt
@@ -3,6 +3,7 @@ package com.example.level2_task1
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.example.level2_task1.databinding.ActivityMainBinding
+import com.example.level2_task1.databinding.ItemPlaceBinding
 import kotlinx.android.synthetic.main.item_place.view.*
 
 class PlaceAdapter {
@@ -10,8 +11,7 @@ class PlaceAdapter {
 
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        // Bind the image and name to the ImageView and TextView
-        val binding = .bind(itemView)
+        val binding = ItemPlaceBinding.bind(itemView)
 
         fun bind(place: Place){
             binding.tvPlace.text = place.name
